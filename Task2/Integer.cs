9b9a8a9d25b2d1c8cb7c57fb0fd011eab3c0bac2
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-06-01 01:42:16 +0300

    Task 2

---
+++ Task2/Integer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Integer
+    {
+        static public int Sum(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        static public int Difference(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
+        static public int Nod(int number1, int number2)
+        {
+            if (number2 < 0) number2 = -number2;
+            if (number1 < 0) number1 = -number1;
+
+            while (number2 > 0)
+            {
+                int temp = number2;
+                number2 = number1 % number2;
+                number1 = temp;
+            }
+            return number1;
+        }
+        static public int Nok(int number1, int number2)
+        {
+            return Math.Abs(number1 * number2) / Nod(number1, number2);
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*
+            Создать программу, которая будет вычислять и выводить на экран 
+            сумму, разность, НОК и НОД двух целых чисел, введенных 
+            пользователем. Если пользователь некорректно введёт хотя бы одно
+            из чисел, то сообщать об ошибке.
+            */
+
+            int[] numbers = new int[2];
+            string[] words = new string[] { "first", "second" };
+
+            for (int elementIndex = 0; elementIndex < numbers.Length; elementIndex++) 
+            {
+                while (true)
+                {
+                    Console.Write("Enter {0} integer --> ", words[elementIndex]);
+                    bool isInteger = Int32.TryParse(Console.ReadLine(), out int number);
+                    if (isInteger)
+                    {
+                        numbers[elementIndex] = number;
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write("ERROR!");
+                    }
+                }
+            }
+
+            int sum = Integer.Sum(numbers[0], numbers[1]);
+            Console.WriteLine("Sum = {0}", sum);
+
+            int difference = Integer.Difference(numbers[0], numbers[1]);
+            Console.WriteLine("Difference = {0}", difference);
+
+            int nod = Integer.Nod(numbers[0], numbers[1]);
+            Console.WriteLine("Nod = {0}", nod);
+
+            int nok = Integer.Nok(numbers[0], numbers[1]);
+            Console.WriteLine("Nok = {0}", nok);
+
+            Console.ReadKey();
+        }
+    }
+}
