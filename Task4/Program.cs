68cff03336ca5f13c44fe468fe1ff92178e5c777
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-06-01 02:22:56 +0300

    Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Создать программу, которая будет подсчитывать количество слов в
+            предложении и выводить их в отсортированном виде. Предложение
+            вводится вручную. (Разделители пробел (“ ”) и запятая (“,”)).
+            */
+
+            Console.Write("Enter the sentence --> ");
+            string sentence = Console.ReadLine();
+            
+            Console.WriteLine("Number of words in sentence -- {0}", Sentence.CountNumberOfWords(sentence));
+
+            Console.WriteLine("Sorted word list:");
+            List<string> w = Sentence.sortWords(sentence);
+            foreach (string word in w)
+            {
+                Console.WriteLine(word);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task4/Sentence.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Sentence
+    {
+        static public int CountNumberOfWords(string sentence)
+        {
+            return sentence.Split(',', ' ').Length;
+        }
+
+        static public List<string> sortWords(string sentence)
+        {
+            List<string> words = new List<string>();
+
+            words.AddRange(sentence.Split(',', ' '));
+            words.Sort();
+
+            return words;
+        }
+    }
+}
