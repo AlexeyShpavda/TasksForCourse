3ac9c79063df57230aa68fd0b8272cdc16198768
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-06-01 01:00:57 +0300

    Task 1

---
+++ Task1/Integer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Integer
+    {
+        public static bool IsEven(int number)
+        {
+            if (number % 2 == 0) return true;
+            else return false;
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number < 2) return false;
+            else
+            {
+                for (int divisor = 2; divisor <= number / 2; divisor++)
+                {
+                    if (number % divisor == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*
+            Создать программу, которая будет сообщать, является ли целое
+            число, введенное пользователем, чётным или нечётным, простым
+            или составным.  Если пользователь введёт не целое число, то
+            сообщать ему об ошибке.
+            */
+
+            int number;
+            while (true)
+            {
+                Console.Write("Enter an integer --> ");
+                bool isInteger = Int32.TryParse(Console.ReadLine(), out number);
+                if (isInteger) break;
+                else
+                {
+                    Console.Write("ERROR!");
+                }
+            }
+
+            bool isEven = Integer.IsEven(number);
+            Console.WriteLine("Is number Even? {0}", isEven);
+            bool isPrime = Integer.IsPrime(number);
+            Console.WriteLine("Is number Prime? {0}", isPrime);
+
+            Console.ReadKey();
+        }
+    }
+}
