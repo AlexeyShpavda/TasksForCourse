432508388531bb30f48da3e6877e8769f749021b
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-06-01 01:52:10 +0300

    Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Необходимо написать программу, которая проверяет слово на 
+            "палиндромность".Слово для проверки вводится вручную во время
+            выполнения программы.
+            */
+
+            string word = Console.ReadLine();
+
+            Console.WriteLine("Is it a palindrome? {0}", Word.isPalindrome(word));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task3/Word.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Word
+    {
+        static public bool isPalindrome(string word)
+        {
+            for (var characterIndex = 0; characterIndex < word.Length / 2; characterIndex++)
+            {
+                if (char.ToLower(word[characterIndex]) != char.ToLower(word[word.Length - characterIndex - 1]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
