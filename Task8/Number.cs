93d6c6415b9202fd5f65ed131a93b9474654f52e
Alexey Shpavda <alexeyshpavda@gmail.com>
2018-06-01 03:27:07 +0300

    Task 8

---
+++ Task8/Number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    class Number
+    {
+        static public bool isPalindrome(string number)
+        {
+            for (var digitIndex = 0; digitIndex < number.Length / 2; digitIndex++)
+            {
+                if (char.ToLower(number[digitIndex]) != char.ToLower(number[number.Length - digitIndex - 1]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Task8/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Написать программу, которая в последовательности от 0 до N,
+            находит все числа-палиндромы(зеркальное значение равно
+            оригинальному).Длина последовательности вводится вручную и не
+            должна превышать 100.
+            */
+            int sizeOfArray;
+            while (true)
+            {
+                Console.Write("Enter an integer less than 100 --> ");
+                bool isInteger = Int32.TryParse(Console.ReadLine(), out sizeOfArray);
+                if (isInteger && (sizeOfArray < 100)) break;
+                else
+                {
+                    Console.Write("ERROR!");
+                }
+            }
+
+            int[] array = new int[sizeOfArray];
+
+            for(var indexOfElement = 0; indexOfElement < sizeOfArray; indexOfElement++)
+            {
+                array[indexOfElement] = 2 * (indexOfElement + 1) - 1;
+            }
+
+            foreach(int el in array)
+            {
+                if (Number.isPalindrome(Convert.ToString(el)))
+                {
+                    Console.WriteLine(el);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
